a3fba299baafe5c2755bde30a31692329bd09c50
unknown <rafles1895@gmail.com>
2024-11-11 01:11:21 -0300

    moved transform folder and created more models

---
+++ dags/clever_transform/models/company_profiles_google_maps/features/company_profiles_features_final.sql
@@ -0,0 +1,16 @@
+{{ config(
+	alias='company_profiles_features',
+	materialized='table',
+	indexes=[
+        {'columns': ['place_id'], 'type': 'btree'},
+		{'columns': ['feature'], 'type': 'btree'}
+    ]
+) }}
+
+SELECT
+	place_id,
+	category,
+	feature,
+	feature_description
+FROM {{ ref('company_profiles_features_unnested') }}
+WHERE COALESCE(feature_description, 'true') <> 'false'
